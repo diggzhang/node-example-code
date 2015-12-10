a425a4f2d675ff61757fdb3112c53c46770ad79d
diggzhang <diggzhang@gmail.com>
2015-12-10 10:04:56 +0800

    one simple node server example

---
+++ simpleServer.js
@@ -0,0 +1,19 @@
+
+var http = require('http');
+
+function handle_incoming_request(req, res) {
+    console.log("INCOMING REQUEST: " + req.method + " " + req.url);
+    res.writeHead(200, { "Content-Type" : "application/json" });
+    res.end(JSON.stringify( {
+            error: null,
+            name: "diggzhang",
+            age: 31
+        }) + "\n");
+}
+
+
+var s = http.createServer(handle_incoming_request);
+
+s.listen(8080);
+
+
